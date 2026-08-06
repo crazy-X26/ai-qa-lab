7c828fb859bc6d95113223e95cccb10940780efe
crazy-X26 <266837956+crazy-X26@users.noreply.github.com>
2026-08-06 19:44:20 +0200

    Introduce login page: add AccountController, LoginViewModel, Login.cshtml, and layout link

---
+++ src/AiQaLab.DemoApp/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AiQALab.DemoApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/AiQaLab.DemoApp/Views/Account/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AiQALab.DemoApp.Views.Accounts
+{
+    public class Login : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
