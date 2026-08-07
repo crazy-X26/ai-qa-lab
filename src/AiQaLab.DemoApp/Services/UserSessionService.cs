a25cdc16496b99c37c1236459c64ef981d42d71d
crazy-X26 <266837956+crazy-X26@users.noreply.github.com>
2026-08-07 14:01:08 +0200

    added placeholder implementation

--- src/AiQaLab.DemoApp/Services/UserSessionService.cs
+++ src/AiQaLab.DemoApp/Services/UserSessionService.cs
@@ -4,5 +4,24 @@ namespace AiQALab.DemoApp.Services
 {
     public class UserSessionService : IUserSessionService
     {
+        public UserSession? GetCurrentUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsAuthenticated()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetCurrentUserSession(string userName, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SignOut()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
