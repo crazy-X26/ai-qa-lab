4ab25fcb208a40786cb8a62a44700cbc96de273e
crazy-X26 <266837956+crazy-X26@users.noreply.github.com>
2026-08-21 15:03:45 +0200

    rename project

---
+++ src/AiQaLab.Cli/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AiQaLab.Cli
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
